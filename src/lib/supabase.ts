a80dc2e51138bddebe1d3330aa7c51d05b83096b
waldejunior Dias <waldejunior2.0@gmail.com>
2026-08-16 08:42:06 -0300

    feat: add Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://ucvutimcfthupaljoxdp.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_ZNgikya1HBy67qA7NSdFYA_zLmdc-9N';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
